a54ee58a627a938ee608c852efe6601e06843d0b
Philip Ye <yestyle@gmail.com>
2016-09-05 21:32:58 +0800

    Initial commit based on https://downloads.wordpress.org/theme/origami.1.7.3.zip

---
+++ inc/contributors.php
@@ -0,0 +1,17 @@
+<?php
+return json_decode( '{
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 3,
+		"score": 0.2551788801804957,
+		"percent": 71.25876484944568
+	},
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 1,
+		"score": 0.10292286452365874,
+		"percent": 28.741235150554324
+	}
+}', true );
