40f6bd6d84fc88947b41e1c69cf8a97deb6207ba
Philip Ye <yestyle@gmail.com>
2017-07-25 09:41:37 +0800

    Upgrade master branch to 1.7.5

--- inc/contributors.php
+++ inc/contributors.php
@@ -3,15 +3,15 @@ return json_decode( '{
 	"b7122312e4008f8f2c76911080bca01a": {
 		"name": "Andrew Misplon",
 		"email": "b7122312e4008f8f2c76911080bca01a",
-		"loc": 75,
-		"score": 8.68976108673088,
-		"percent": 71.92556633286125
+		"loc": 201,
+		"score": 15.713748112000154,
+		"percent": 81.70115921673028
 	},
 	"a885c7bd5442dd2acd8c3e42001332c6": {
 		"name": "Alex S",
 		"email": "a885c7bd5442dd2acd8c3e42001332c6",
-		"loc": 36,
-		"score": 3.39184150575467,
-		"percent": 28.07443366713874
+		"loc": 38,
+		"score": 3.519452815193527,
+		"percent": 18.298840783269718
 	}
 }', true );
